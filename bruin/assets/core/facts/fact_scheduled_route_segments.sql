dbf7d6bdf0b84452a936f10e002712c21a928936
levi <levi.devhaus@gmail.com>
2026-09-20 10:23:42 +0930

    fix bug ambiguity column in fact_scheduled_route_segments.sql

--- bruin/assets/core/facts/fact_scheduled_route_segments.sql
+++ bruin/assets/core/facts/fact_scheduled_route_segments.sql
@@ -13,14 +13,14 @@ depends:
 WITH ordered_events AS (
   SELECT
     *,
-    LEAD(stop_id) OVER (PARTITION BY trip_id ORDER BY stop_sequence) AS to_stop_id,
-    LEAD(stop_name) OVER (PARTITION BY trip_id ORDER BY stop_sequence) AS to_stop_name,
-    LEAD(stop_lat) OVER (PARTITION BY trip_id ORDER BY stop_sequence) AS to_stop_lat,
-    LEAD(stop_lon) OVER (PARTITION BY trip_id ORDER BY stop_sequence) AS to_stop_lon,
-    LEAD(lga_name) OVER (PARTITION BY trip_id ORDER BY stop_sequence) AS to_lga_name,
-    LEAD(stop_sequence) OVER (PARTITION BY trip_id ORDER BY stop_sequence) AS to_stop_sequence,
-    LEAD(scheduled_arrival_seconds) OVER (PARTITION BY trip_id ORDER BY stop_sequence) AS scheduled_arrival_seconds,
-    LEAD(shape_dist_traveled) OVER (PARTITION BY trip_id ORDER BY stop_sequence) AS to_shape_dist_traveled
+    LEAD(stop_id) OVER (PARTITION BY trip_id ORDER BY stop_sequence) AS next_stop_id,
+    LEAD(stop_name) OVER (PARTITION BY trip_id ORDER BY stop_sequence) AS next_stop_name,
+    LEAD(stop_lat) OVER (PARTITION BY trip_id ORDER BY stop_sequence) AS next_stop_lat,
+    LEAD(stop_lon) OVER (PARTITION BY trip_id ORDER BY stop_sequence) AS next_stop_lon,
+    LEAD(lga_name) OVER (PARTITION BY trip_id ORDER BY stop_sequence) AS next_lga_name,
+    LEAD(stop_sequence) OVER (PARTITION BY trip_id ORDER BY stop_sequence) AS next_stop_sequence,
+    LEAD(scheduled_arrival_seconds) OVER (PARTITION BY trip_id ORDER BY stop_sequence) AS next_scheduled_arrival_seconds,
+    LEAD(shape_dist_traveled) OVER (PARTITION BY trip_id ORDER BY stop_sequence) AS next_shape_dist_traveled
   FROM `adelaide-metro-505702.core.fact_scheduled_stop_events`
 ),
 
@@ -37,26 +37,26 @@ paired_events AS (
     route_type_name,
     agency_id,
     stop_sequence AS from_stop_sequence,
-    to_stop_sequence,
+    next_stop_sequence AS to_stop_sequence,
     stop_id AS from_stop_id,
     stop_name AS from_stop_name,
     stop_lat AS from_stop_lat,
     stop_lon AS from_stop_lon,
     lga_name AS from_lga_name,
-    to_stop_id,
-    to_stop_name,
-    to_stop_lat,
-    to_stop_lon,
-    to_lga_name,
+    next_stop_id AS to_stop_id,
+    next_stop_name AS to_stop_name,
+    next_stop_lat AS to_stop_lat,
+    next_stop_lon AS to_stop_lon,
+    next_lga_name AS to_lga_name,
     scheduled_departure_seconds,
-    scheduled_arrival_seconds,
+    next_scheduled_arrival_seconds AS scheduled_arrival_seconds,
     departure_hour,
     departure_time_bucket,
     shape_dist_traveled AS from_shape_dist_traveled,
-    to_shape_dist_traveled
+    next_shape_dist_traveled AS to_shape_dist_traveled
   FROM ordered_events
-  WHERE to_stop_id IS NOT NULL
-    AND stop_id != to_stop_id
+  WHERE next_stop_id IS NOT NULL
+    AND stop_id != next_stop_id
 )
 
 SELECT
